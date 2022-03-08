b4d020b841050b82745a3f76a4261f15d008484f
huLasPet <kenwoodo@hotmail.com>
2022-03-08 11:02:01 +0100

    Python_Project_GUI

--- Tying speed test/main.py
+++ Tying speed test/main.py
@@ -9,6 +9,7 @@ class TypingSpeed:
         self.displayed_text = ""
         self.typed_chars = 0
         self.start = 0
+        self.stop = 0
 
     def get_sample_text(self):
         response = requests.get(url="https://corporatebs-generator.sameerkumar.website/")
@@ -24,7 +25,7 @@ class TypingSpeed:
         self.displayed_text = self.get_sample_text()
         text_display.config(text=self.displayed_text)
         text_entry.config(width=len(self.displayed_text))
-        text_entry.grid(column=0, row=1, sticky="w")
+        text_entry.grid(column=1, row=1, sticky="w")
 
     def stop_typing(self):
         self.stop = time.time()
@@ -33,19 +34,22 @@ class TypingSpeed:
         text_display.config(text=f"Typed in {self.typed_chars} characters in {time_diff:.2} seconds.\n"
                                  f"This means your typing speed is {(self.typed_chars / time_diff):.2} chars a second.")
 
+
 typing = TypingSpeed()
 window = tk.Tk()
 window.config(padx=20, pady=20)
 window.title("Typing speed")
+window.bind('<Return>', lambda event: typing.display_text_and_entry())
+window.bind('<Escape>', lambda event: typing.stop_typing())
 
-text_display = tk.Label(text="Press start")
-text_display.grid(column=0, row=0)
+text_display = tk.Label(text="Press Start or Enter to start\n"
+                             "Get new text by pressing either\n"
+                             "To finish press ESC")
+text_display.grid(column=1, row=0)
 text_entry = tk.Entry(window)
 
 
 start_button = tk.Button(text="Start", command=typing.display_text_and_entry)
 start_button.grid(column=0, row=2, sticky="w")
-stop_button = tk.Button(text="Stop", command=typing.stop_typing)
-stop_button.grid(column=1, row=2, sticky="w")
 
 window.mainloop()
