d2ecfc59d9d367db92b2a8b7d70ae9a1a6167c90
huLasPet <kenwoodo@hotmail.com>
2022-03-11 08:07:25 +0100

    Python_Project_GUI

--- Typing speed test/main.py
+++ Typing speed test/main.py
@@ -19,14 +19,14 @@ class TypingSpeed:
         return self.bs_text
 
     def display_text_and_entry(self):
-        """Starts the timer and displays the text to be entered when the Start button or Enter is pressed.
+        """Starts the timer and displays the text to be entered when Enter is pressed.
         Text color changes to red if there is a typo."""
         if self.start == 0:
             self.start = time.time()
         if self.displayed_text == "" or self.displayed_text == text_entry.get():
             self.typed_chars += len(text_entry.get())
             self.displayed_text = self.get_sample_text()
-            text_display.config(text=self.displayed_text)
+            text_display.config(text=self.displayed_text, fg="black")
             text_entry.delete(0, "end")
             text_entry.config(width=len(self.displayed_text))
             text_entry.grid(column=1, row=1, sticky="w")
@@ -52,13 +52,11 @@ window.config(padx=20, pady=20)
 window.title("Typing speed")
 window.bind('<Return>', lambda event: typing.display_text_and_entry())
 window.bind('<Escape>', lambda event: typing.stop_typing())
-text_display = tk.Label(text="Press Start or Enter to start\n"
-                             "Get new text by pressing either\n"
+text_display = tk.Label(text="Press Enter to start\n"
+                             "To submit the text and get a new press Enter\n"
                              "If the text changes to red there is a typo\n"
                              "To finish press ESC")
 text_display.grid(column=1, row=0)
 text_entry = tk.Entry(window)
-start_button = tk.Button(text="Start", command=typing.display_text_and_entry)
-start_button.grid(column=0, row=2, sticky="w")
 
 window.mainloop()
