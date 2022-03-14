82bc82edc7b37a32c34cb5b264c3eb8201d4a15e
huLasPet <kenwoodo@hotmail.com>
2022-03-14 09:19:48 +0100

    Python_Project_GUI

--- Disappearing text/main.py
+++ Disappearing text/main.py
@@ -2,13 +2,27 @@
 #Try to check if the length of the entry changed and if not, delete what is there
 
 import tkinter as tk
-import time
 
+
+class TextDelete:
+    def __init__(self):
+        self.char_count = 0
+        self.prev_count = 0
+
+    def delete(self):
+        self.prev_count = self.char_count
+        self.char_count = len(text_display.get(1.0, 'end'))
+        if self.prev_count == self.char_count:
+            text_display.delete(1.0, 'end')
+        window.after(5000, self.delete)
+
+
+delete_class = TextDelete()
 window = tk.Tk()
 window.config(padx=20, pady=20)
 window.title("Disappearing text")
 text_display = tk.Text(window)
-text_display.insert('end', "ASD")
-text_display.grid(column=1, row=0)
-print(text_display.get(1.0, 'end'))
+text_display.insert('end', "Stop typing for 5 seconds and lose what you have typed")
+text_display.grid(column=0, row=0)
+delete_class.delete()
 window.mainloop()
