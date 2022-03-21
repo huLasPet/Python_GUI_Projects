fd615f0f060179b1356802792de5d008f8f87ed6
huLasPet <kenwoodo@hotmail.com>
2022-03-21 09:42:04 +0100

    Python_Project_GUI

--- T-Rex automated/main.py
+++ T-Rex automated/main.py
@@ -1,19 +1,27 @@
-#https://elgoog.im/t-rex/
+import os
 import pyautogui
-from PIL import Image
-
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
 import time
+from dotenv import load_dotenv
 
+load_dotenv(r"C:\Users\hulaspet\DEV\Python_env\.env")
+CHROME_DRIVER_PATH = ChromeService(executable_path=os.getenv("chrome_driver"))
+LINK_TO_SITE = "https://elgoog.im/t-rex/"
+
+driver = webdriver.Chrome(service=CHROME_DRIVER_PATH)
+driver.get(LINK_TO_SITE)
+driver.maximize_window()
 
 time.sleep(5)
-print("Start")
+pyautogui.press('up')
 while True:
     #1st way
     # jump = pyautogui.locateOnScreen("not_clear.PNG", confidence=0.95)
     # if jump == None:
-    #     pyautogui.press("space")
+    #     pyautogui.press("up")
     #2nd way
-    jumper = pyautogui.pixelMatchesColor(820, 398, (83, 83, 83))
+    jumper = pyautogui.pixelMatchesColor(720, 550, (83, 83, 83))
     if jumper:
-        pyautogui.press('space')
+        pyautogui.press('up')
 
